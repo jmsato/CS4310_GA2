b07b68d50620134c1017f46940b30aed725ce0ff
jmsato <jordyn.sato@gmail.com>
2021-03-26 15:08:45 -0700

    Created Process class

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,57 @@
 
 public class Process {
 
+	private boolean active;
+	private int arrivalTime;
+	private int totalCPUTime;
+	private int remainingCPUTime;
+	private int turnaroundTime;
+	
+	public Process(boolean active, int arrivalTime, int totalCPUTime) {
+		this.active = active;
+		this.arrivalTime = arrivalTime;
+		this.totalCPUTime = totalCPUTime;
+		this.remainingCPUTime = totalCPUTime;
+		this.turnaroundTime = 0;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(int arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public int getTotalCPUTime() {
+		return totalCPUTime;
+	}
+
+	public void setTotalCPUTime(int totalCPUTime) {
+		this.totalCPUTime = totalCPUTime;
+	}
+
+	public int getRemainingCPUTime() {
+		return remainingCPUTime;
+	}
+
+	public void setRemainingCPUTime(int remainingCPUTime) {
+		this.remainingCPUTime = remainingCPUTime;
+	}
+
+	public int getTurnaroundTime() {
+		return turnaroundTime;
+	}
+
+	public void setTurnaroundTime(int turnaroundTime) {
+		this.turnaroundTime = turnaroundTime;
+	}
 }
